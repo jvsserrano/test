71df38632b8ae485caffa2764323c18e1c0702c7
Juan Vicente Serrano Soler <hpsw_ID@HPSW-ID.Clase10.com>
2018-03-01 16:18:38 +0100

    Ver4

--- skills/command-help.js
+++ skills/command-help.js
@@ -6,10 +6,10 @@ module.exports = function (controller) {
     controller.hears(["help", "who"], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
         text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
-        text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
+        text += "\n- " + bot.enrichCommand(message, "help") + ": Ayuda";
         text += "\n- " + bot.enrichCommand(message, "espacio") + ": espacio clonado";
-        text += "\n- " + bot.enrichCommand(message, "pedido") + ": espacio clonado";
-        text += "\n- " + bot.enrichCommand(message, "add") + ": espacio clonado";
+        text += "\n- " + bot.enrichCommand(message, "pedido") + ": informacion del pedido";
+        text += "\n- " + bot.enrichCommand(message, "add") + ": Añade usuario a la sala actual";
         bot.reply(message, text);
     });
 }
